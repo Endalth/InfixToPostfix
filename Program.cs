fda33e3228177b463519620f58c9c2c6b5f6763b
Endalth <alican__karamaras@hotmail.com>
2020-05-27 20:04:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InfixToPostfix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> myStack = new Stack<char>();
+
+            string operators = "-+*/^()";
+            string[] expressions = new string[]
+            {
+                "A^B^C",
+                "A*(B+C)",
+                "A-B*C^D",
+                "A+B*C-D*E",
+                "((A+B)*C-D)*E"
+            };
+
+            foreach (string expression in expressions)
+            {
+                string postfixString = "";
+                Console.Write(expression + " -> ");
+                for (int i = 0; i < expression.Length; i++)
+                {
+                    char character = expression[i];
+                    if (!operators.Contains(expression[i]))
+                    {
+                        postfixString += character;
+                    }
+                    else
+                    {
+                        if (character == '(')
+                            myStack.Push(character);
+                        else if (character == ')')
+                        {
+                            while (myStack.Peek() != '(')
+                            {
+                                postfixString += myStack.Pop();
+                            }
+                            myStack.Pop(); //to remove '('
+                        }
+                        else
+                        {
+                            while (myStack.Count > 0 && HasHigherOrEqualPrec(myStack.Peek(), character))
+                            {
+                                postfixString += myStack.Pop();
+                            }
+                            myStack.Push(character);
+                        }
+                    }
+                }
+
+                //append what is left inside the stack to postfixString
+                while (myStack.Count > 0)
+                {
+                    postfixString += myStack.Pop();
+                }
+
+                Console.WriteLine(postfixString);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static bool HasHigherOrEqualPrec(char first, char second)
+        {
+            //exponent is right to left that is why we don't want it to pop itself
+            //without this if statement 2^3^4 would be 23^4^ and this is wrong
+            if (first == '^' && second == '^')
+                return false;
+
+            Dictionary<char, int> precedence = new Dictionary<char, int>
+            {
+                {'(', 0 }, //we don't want anything to pop '(' that is why it is 0
+                {'^', 3 },
+                {'*', 2 },
+                {'/', 2 },
+                {'+', 1 },
+                {'-', 1 }
+            };
+
+            return precedence[first] >= precedence[second];
+        }
+    }
+}
